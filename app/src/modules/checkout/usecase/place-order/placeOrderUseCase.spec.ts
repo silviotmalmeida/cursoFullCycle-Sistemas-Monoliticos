778494ceedbb4303ee64e50ff1e6a7ff3d9efdf2
silviotmalmeida <silviotmalmeida@gmail.com>
2023-05-18 18:05:16 -0300

    Implementação até aula 69

--- app/src/modules/checkout/usecase/place-order/placeOrderUseCase.spec.ts
+++ app/src/modules/checkout/usecase/place-order/placeOrderUseCase.spec.ts
@@ -1,8 +1,10 @@
 // dependências
+import Id from "../../../@shared/domain/value-object/id";
 import { FindClientFacadeOutputDto } from "../../../client-adm/facade/clientAdmFacadeInterface";
 import { CheckStockFacadeOutputDto } from "../../../product-adm/facade/productAdmFacadeInterface";
 import { FindStoreCatalogFacadeOutputDto } from "../../../store-catalog/facade/storeCatalogFacadeInterface";
-import { PlaceOrderInputDto } from "./placeOrderDto";
+import Product from "../../domain/product";
+import { PlaceOrderInputDto, PlaceOrderOutputDto } from "./placeOrderDto";
 import PlaceOrderUseCase from "./placeOrderUseCase";
 
 // suíde de testes de unidade do caso de uso
@@ -11,7 +13,7 @@ describe("Place Order Usecase unit test", () => {
   // @ts-expect-error - no params in constructor
   const usecase = new PlaceOrderUseCase();
 
-  // teste relacionados ao método validateProducts
+  // testes relacionados ao método validateProducts
   describe("validateProducts method", () => {
     // se os produtos não forem selecionados, deve-se lançar um erro
     it("should throw an error when no products are selected", async () => {
@@ -91,7 +93,7 @@ describe("Place Order Usecase unit test", () => {
     });
   });
 
-  // teste relacionados ao método getProducts
+  // testes relacionados ao método getProducts
   describe("getProducts method", () => {
     // criando a data mocada
     const mockDate = new Date(2000, 1, 1);
@@ -129,6 +131,39 @@ describe("Place Order Usecase unit test", () => {
       await expect(usecase["getProduct"]("0")).rejects.toThrow(
         new Error("Product 0 not found")
       );
+    });
+
+    // deve-se retornar produtos
+    it("should return a product", async () => {
+      // criando o mock retorno do método find da catalogFacade
+      const catalog: FindStoreCatalogFacadeOutputDto = {
+        id: "0",
+        name: "Product 0",
+        description: "Product 0 description",
+        salesPrice: 0,
+      };
+
+      // configurando o mock da CatalogFacade
+      const MockCatalogFacade = {
+        find: jest.fn().mockResolvedValue(catalog), // o método find retorna o produto criado
+      };
+
+      // padrão do objeto Product a ser retornado pelo getProduct
+      const outputProduct = new Product({
+        Id: new Id("0"),
+        name: "Product 0",
+        description: "Product 0 description",
+        salesPrice: 0,
+      });
+
+      // forçando injeção da catalogFacade
+      // @ts-expect-error - force set clientFacade
+      usecase["_catalogFacade"] = MockCatalogFacade;
+
+      // verificando a execução
+      // executando o método privado getProduct diretamente
+      await expect(usecase["getProduct"]("0")).resolves.toEqual(outputProduct);
+      expect(MockCatalogFacade.find).toHaveBeenCalledTimes(1);
     });
   });
 
@@ -176,7 +211,7 @@ describe("Place Order Usecase unit test", () => {
       // @ts-expect-error - force set clientFacade
       usecase["_clientFacade"] = MockClientFacade;
 
-      // espionando a execução do método privado interno do usecase
+      // mocando a execução do método privado interno do usecase
       const mockValidateProducts = jest
         // @ts-expect-error - spy on private method
         .spyOn(usecase, "validateProducts")
@@ -194,6 +229,140 @@ describe("Place Order Usecase unit test", () => {
         new Error("No products selected")
       );
       expect(mockValidateProducts).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("place order", () => {
+    // const clientProps = {
+    //   Id: "1c",
+    //   name: "Client 0",
+    //   document: "0000",
+    //   email: "client@user.com",
+    //   street: "some address",
+    //   number: "1",
+    //   complement: "",
+    //   city: "some city",
+    //   state: "some state",
+    //   zipCode: "000",
+    // };
+
+    // criando o mock retorno do método find da clientFacade
+    const client: FindClientFacadeOutputDto = {
+      id: "1c",
+      name: "Client 0",
+      email: "client@user.com",
+      address: "Address 1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    // configurando o mock da ClientFacade
+    const MockClientFacade = {
+      add: jest.fn().mockResolvedValue(null), // o método add retorna null
+      find: jest.fn().mockResolvedValue(client), // o método find retorna o cliente mocado
+    };
+
+    // configurando o mock da PaymentFacade
+    const MockPaymentFacade = {
+      process: jest.fn(), // o método process não faz nada
+    };
+
+    // configurando o mock do CheckoutGateway
+    const MockCheckoutGateway = {
+      addOrder: jest.fn(), // o método addOrder não faz nada
+      findOrder: jest.fn().mockResolvedValue(null), // o método findOrder retorna null
+    };
+
+    // configurando o mock da InvoiceFacade
+    const MockInvoiceFacade = {
+      generate: jest.fn().mockResolvedValue({ id: "1i" }), // o método generate retorna um objeto com id
+      find: jest.fn().mockResolvedValue(null), // o método find retorna null
+    };
+
+    // criando o caso de uso
+    const usecase = new PlaceOrderUseCase(
+      MockClientFacade,
+      null,
+      null,
+      MockInvoiceFacade,
+      MockPaymentFacade,
+      MockCheckoutGateway
+    );
+
+    const products = {
+      "1": new Product({
+        Id: new Id("1"),
+        name: "Product 1",
+        description: "some description",
+        salesPrice: 40,
+      }),
+      "2": new Product({
+        Id: new Id("2"),
+        name: "Product 2",
+        description: "some description",
+        salesPrice: 30,
+      }),
+    };
+
+    // mocando a execução do método privado interno do usecase
+    const mockValidateProducts = jest
+      // @ts-expect-error - spy on private method
+      .spyOn(usecase, "validateProducts")
+      // @ts-expect-error - not return never
+      // o validate vai retornar null
+      .mockResolvedValue(null);
+
+    // mocando a execução do método privado interno do usecase
+    const mockGetProduct = jest
+      // @ts-expect-error - spy on private method
+      .spyOn(usecase, "getProduct")
+      // @ts-expect-error - not return never
+      // o get vai retornar um array com os id dos produtos
+      .mockImplementation((productId: keyof typeof products) => {
+        return products[productId];
+      });
+
+    // caso não seja aprovada
+    it("should not be approved", async () => {
+      // alterando o comportamento da MockPaymentFacade
+      MockPaymentFacade.process = MockPaymentFacade.process.mockReturnValue({
+        transactionId: "1t",
+        orderId: "1o",
+        amount: 100,
+        status: "error",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      // definindo o input do caso de uso
+      const input: PlaceOrderInputDto = {
+        clientId: "1c",
+        products: [{ productId: "1" }, { productId: "2" }],
+      };
+
+      // executando o caso de uso
+      let output: PlaceOrderOutputDto = await usecase.execute(input);
+
+      // verificando a execução
+      expect(output.invoiceId).toBeNull();
+      expect(output.total).toBe(70);
+      expect(output.products).toStrictEqual([
+        { productId: "1" },
+        { productId: "2" },
+      ]);
+      expect(MockClientFacade.find).toHaveBeenCalledTimes(1);
+      expect(MockClientFacade.find).toHaveBeenCalledWith({ id: "1c" });
+      expect(MockClientFacade.find).toHaveBeenCalledTimes(1);
+      expect(mockValidateProducts).toHaveBeenCalledTimes(1);
+      expect(mockValidateProducts).toHaveBeenCalledWith(input);
+      expect(mockGetProduct).toHaveBeenCalledTimes(2);
+      expect(MockCheckoutGateway.addOrder).toHaveBeenCalledTimes(1);
+      expect(MockPaymentFacade.process).toHaveBeenCalledTimes(1);
+      expect(MockPaymentFacade.process).toHaveBeenCalledWith({
+        orderId: output.id,
+        amount: output.total,
+      });
+      expect(MockInvoiceFacade.generate).toHaveBeenCalledTimes(0);
     });
   });
 
